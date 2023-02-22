65156b1358971bfa4ed70fc3fdb44703e8ea142d
bendms <97233634+bendms@users.noreply.github.com>
2023-02-22 11:29:55 +0100

    add models for Customer Contract Event

--- epicevents/api/models.py
+++ epicevents/api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from epicevents.settings import AUTH_USER_MODEL as MyUser
 
-# Create your models here.
+class Customer(models.Model):
+    
+    firstname = models.CharField(max_length=25, blank=True)
+    lastname = models.CharField(max_length=25, blank=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    mobile = models.CharField(max_length=20, blank=True)
+    company_name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    sales_contact = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.company_name + " " + self.firstname + " " + self.lastname
+
+class Contract(models.Model):
+    
+    sales_contact = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    amount = models.FloatField()
+    payment_due = models.DateTimeField()
+    
+    def __str__(self):
+        return "Contrat : " + self.customer + " Montant : " + self.amount
+
+class EventStatus(models.Model):
+    
+    EVENT_STATUS_CHOICES = [
+        ("PLANNED", "Planifié"),
+        ("ACCOMPLISHED", "Réalisé"),
+    ]    
+    
+    status = models.CharField(max_length=50, choices=EVENT_STATUS_CHOICES)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+class Event(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    support_contact = models.ForeignKey(MyUser, on_delete=models.DO_NOTHING)
+    event_status = models.ForeignKey(EventStatus, on_delete=models.CASCADE)
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField()
+    notes = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "Évènement pour " + self.customer + " en date du " + self.event_date
+
